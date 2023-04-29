b36d4a37ed8239fde45f4677a295ecf7da9787c6
RA <103623573+rhyannaaz@users.noreply.github.com>
2023-04-29 15:18:09 +1000

    Added json file

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+* Authors: Hayden Tran (102882815), Rhyanna Arisya Zaharom (103698709)
+* Target: index.html
+* Purpose:
+* Created: 29th April 2023
+**/
+
+function init() {
+
+}
+
+window.onload = init;
